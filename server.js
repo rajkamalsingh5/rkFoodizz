f6adce6726e26264d596e609e5802863ab2c4f0f
rajkamal <saurabhsingh75555@gmail.com>
2024-05-12 17:58:04 +0530

    initial

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ import cors from "cors";
 import { connectDB } from "./config/database.js";
 import Razorpay from "razorpay";
 connectDB();
+app.options("",cors({
+  credentials: true,
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(
   cors({
     credentials: true,
@@ -10,7 +15,6 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-
 // app.use(cors());
 export const  instance = new Razorpay({ 
   key_id: process.env.RAZORPAY_API_KEY, 
