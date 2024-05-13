6babb794daa3ad07a2da2413171c4dc0bed10931
rajkamal <saurabhsingh75555@gmail.com>
2024-05-13 20:32:10 +0530

    ready to fly

--- utils/Provider.js
+++ utils/Provider.js
@@ -8,7 +8,8 @@ export  const connectPassport=()=>
         passport.use(new GoogleStrategy({
             clientID:process.env.GOOGLE_CLIENT_ID,
             clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL:process.env.GOOGLE_CALLBACK_URL
+            callbackURL:process.env.GOOGLE_CALLBACK_URL,
+            scope:["profile"],
         },async function(accessToken,refreshToken,profile,done){
 
             const user=await User.findOne({
